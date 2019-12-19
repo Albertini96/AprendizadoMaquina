82b0e5628d9905bce7ab3a6ad0a8d118d6b0a0b4
Albertini96 <guilhermeoliveira@arttu.co>
2019-12-19 13:24:31 -0300

    Convolutional Neural Network

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import PCA
 import LDA
 import KMeans
 import Perceptron
+from tensorflow import keras
+
 
 root = '../Bases/'
 #ds = pd.read_excel(root + 'ShoeSize.xlsx').values.tolist()
@@ -24,7 +26,7 @@ root = '../Bases/'
 #ds = pd.read_excel(root + 'Wholesale.xlsx').values
 #ds = pd.read_excel(root + 'user_model.xlsx').values
 #ds = pd.read_excel(root + 'and.xlsx').values
-ds = pd.read_excel(root + 'or.xlsx').values
+#ds = pd.read_excel(root + 'or.xlsx').values
 #ds = pd.read_excel(root + 'xor.xlsx').values
 
 
@@ -135,7 +137,7 @@ ds = pd.read_excel(root + 'or.xlsx').values
 # #Exercicio 5 - Perceptron
 # =============================================================================
 
-weights = Perceptron.fit(ds, 50, 0.1)
+#weights = Perceptron.fit(ds, 50, 0.1)
 #print(Perceptron.predict([0,1], weights))
 
 
@@ -146,6 +148,64 @@ weights = Perceptron.fit(ds, 50, 0.1)
 # =============================================================================
 # #Exercicio 7 - Convolutional Neural Networks
 # =============================================================================
+
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = np.array(x_train) / 255
+x_test = np.array(x_test) / 255
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+#Define uma rede neural sequencial
+lenet = keras.Sequential()
+#Define uma camada de convolução 
+lenet.add(keras.layers.Conv1D(filters=6, 
+                        kernel_size=3, 
+                        activation='relu', 
+                        use_bias=True,
+                        input_shape=(28,28)))
+#Define uma camada de pooling 
+lenet.add(keras.layers.MaxPooling1D(strides=2,
+                                    pool_size=2))
+#Define uma camada de convolução 
+lenet.add(keras.layers.Conv1D(filters=16, 
+                        kernel_size=3, 
+                        activation='relu'))
+#Define uma camada de pooling
+lenet.add(keras.layers.MaxPooling1D(strides=2,
+                                    pool_size=2))
+#Transformação de matrizes em um vetor de entrada para uma MLP
+lenet.add(keras.layers.Flatten())
+#Define uma camada de uma MLP com 120 neurônios
+lenet.add(keras.layers.Dense(units=120, activation='relu'))
+#Define uma camada de uma MLP com 84 neurônios
+lenet.add(keras.layers.Dense(units=84, activation='relu'))
+#Define uma camada de uma MLP com 10 neurônios
+lenet.add(keras.layers.Dense(units=10, activation = 'softmax'))
+
+#Definições de hiper parâmetro
+lenet.compile(
+    loss=keras.losses.categorical_crossentropy,
+    optimizer=keras.optimizers.Adadelta(),
+    metrics=['accuracy']
+)
+
+lenet.fit(x_train, y_train, epochs=10, batch_size=128)
+
+score = lenet.evaluate(x_test, y_test, verbose=0)
+print('Final loss:', score[0])
+print('Final acc:', score[1])
+
+
+
+
+
+
+
+
+
 
 
 
